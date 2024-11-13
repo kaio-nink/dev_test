c7f89427f06eaa12ebe641a3612613a976181210
kaio-nink <kaionink@gmail.com>
2024-11-13 16:41:13 -0300

    feat: definindo entidade User

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,17 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-//TODO Crie a entidade de User
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  first_name!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  last_name!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  email!: string;
+}
+
