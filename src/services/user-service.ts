66649c695f6d7faafc2966a40a7864ac3d420896
kaio-nink <51462685+kaio-nink@users.noreply.github.com>
2024-11-17 17:44:08 -0300

    feat(user-service): throw BadRequestError if an email is already in use before creating a new user

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,12 +1,13 @@
 import { UserDTO } from '../dtos/user-dto';
 import { UserRepository } from '../repositories/user-repository';
+import { BadRequestError } from '../utils/errors/bad-request-error';
 
 export class UserService {
   constructor(private userRepository: UserRepository) {}
 
   async createUser(user: UserDTO) {
     const existingUser = await this.userRepository.getByEmail(user.email);
-    if (existingUser) throw new Error('This email is already in use.');
+    if (existingUser) throw new BadRequestError('This email is already in use.');
 
     const createdUser = await this.userRepository.create(user);
     return createdUser;
